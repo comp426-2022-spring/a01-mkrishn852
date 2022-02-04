1b01b813e20743e23157c59a30b40ba5ae5d84a1
Maya Krishnamoorthy <mkrishn@email.unc.edu>
2022-02-04 14:12:43 -0500

    submit a01

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 // Require http module
-
 const http = require('http')
 
 // Require fs module
-
 const fs = require('fs')
 
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
-const arg = require('minimist')(process.argv.slice(2))
+const args = require('minimist')(process.argv.slice(2))
 
 // Define allowed argument name 'port'.
-
-arg['port']
+args['port']
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
 const port = args.port || process.env.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
@@ -25,16 +20,16 @@ const port = args.port || process.env.port || 3000
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
-fs.readFile('./www/index.html', 'utf8', (err, data) => {
-
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
 
-if (err) {
-    console.error(err)
-    return 
-    process.exit(1)
+fs.readFile('./www/index.html', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+      process.exit(1)
 }
+
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -49,6 +44,7 @@ const server = http.createServer((req, res) => {
     res.end(data)
 })
 
+
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
@@ -56,5 +52,6 @@ server.listen(port, () => {
     console.log(`Server running at port ${port}`)
   })
 })
+
 
 // That's it! You're all done!
