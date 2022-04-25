470faaa2b797843bed82d6a8fddee94976618128
Maya Krishnamoorthy <mkrishn@email.unc.edu>
2022-04-25 14:14:51 -0400

    syntax

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ fs.readFile('./www/index.html', 'utf8' , (err, data) => {
 }
 
 
-
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
